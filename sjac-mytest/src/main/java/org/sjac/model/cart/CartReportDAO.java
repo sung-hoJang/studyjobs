6037ad6a81a4f95172f3a9ee230af87daef814f0
godaewon <godaewon@10.211.55.3>
2015-12-18 17:15:27 +0900

    카트리포트 주석

--- sjac-mytest/src/main/java/org/sjac/model/cart/CartReportDAO.java
+++ sjac-mytest/src/main/java/org/sjac/model/cart/CartReportDAO.java
@@ -4,17 +4,21 @@ import java.util.List;
 import java.util.Map;
 
 public interface CartReportDAO {
-
-	/*public Boolean checkKeyWord(String cval);
-
-	public void insertKeyWord(String cval);
-
-	public void countUpKeyWord(String cval);*/
-
+	/**
+	 * 회원이 그룹을 찜한 횟수 보여주기
+	 * @return
+	 */
 	public List<CartReportVO> showCountCart();
-
+	/**
+	 * 회원이 그룹을 찜한 횟수 업데이트
+	 * @param gleaderId
+	 * @return
+	 */
 	public int updateCartReport(String gleaderId);
-
+	/**
+	 * 회원이 그룹을 찜한 횟수 처음 등록 할때 
+	 * @param gleaderId
+	 */
 	public void insertCartReport(String gleaderId);
 	
 	
--- sjac-mytest/src/main/java/org/sjac/model/cart/CartReportService.java
+++ sjac-mytest/src/main/java/org/sjac/model/cart/CartReportService.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Map;
 
 public interface CartReportService {
-
-	/* public void countKeyWord(String cval); */
-
+	/**
+	 *  회원이 찜한 그룹 갯수 보여주기
+	 * @return
+	 */
 	public List<CartReportVO> showCountCart();
-
+		/**
+		 * 이미 찜했던 그룹일 경우 찜 조회수를 1 높여주고
+		 * 새로운 그룹경우 찜하기 기록에 새로 저장해준다.
+		 * @param gleaderId
+		 */
 	public void saveCartReport(String gleaderId);
 
 }
