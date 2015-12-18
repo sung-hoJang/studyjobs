dee6e01b91e6d6f4c99ad6dddee8da6a808782ab
neung <neung@DESKTOP-M0965RN>
2015-12-18 17:08:28 +0900

    Transaction - 그룹삭제, 회원삭제 수정

--- sjac-mytest/src/main/java/org/sjac/model/TransactionService.java
+++ sjac-mytest/src/main/java/org/sjac/model/TransactionService.java
@@ -6,7 +6,28 @@ import java.util.List;
 public interface TransactionService {
 	public abstract void TransactionAll(GroupVO gvo, GroupMemberVO gmvo) throws Exception;
 	public abstract List<ScheduleVO> ScheduleTransaction(String id) throws Exception;
+	/**
+	 * 			<그룹 삭제>
+	 *       //찜하기 삭제(그룹장 아이디로 검색)
+	 *       //그룹게시판 시퀀스 삭제(그룹장 아이디로 검색) 
+	 *       //그룹보드 삭제(그룹장 아이디로 검색)
+	 *       //라스트 쉐쥴 테이블에서 라스트 그룹스케쥴 삭제(그룹장 아이디로 검색)
+	 *       //그룹 조인 테이블에서 삭제(그룹장 아이디로 검색)
+	 *       //그룹멤버 테이블에서 그룹멤버 삭제(그룹장 아이디로 검색)
+	 *       //그룹테이블에서 그룹삭제(그룹장 아이디로 검색)
+	 * @param id
+	 */
 	public abstract void deleteGroupTransaction(String id);
+	
+	/**
+	 * 			<회원삭제>
+	 * 		 //찜하기 삭제(회원 아이디로 검색)
+	 *  	//그룹멤버 테이블에서 그룹멤버 삭제(회원 아이디로 검색)
+	 *  	//그룹 조인 테이블에서 삭제(회원 아이디로 검색)
+	 *  	//DELETE멤버 테이블로 회원정보 이동(회원 아이디로 검색)
+	 *  	//멤버 테이블에서 회원정보 삭제(회원 아이디로 검색) 
+	 * @param id
+	 */
 	public abstract void deleteMemberTransaction(String id);
 	public abstract void joinGroupTransaction(GroupJoinVO gjvo);
 	public abstract void joinTransaction(String gLeaderId, String[] acceptList);
